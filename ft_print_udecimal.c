9b75e530e283576e854d7214bcbe2ce63131630a
WaPoco1994 <vasili-pogorelov@web.de>
2024-12-05 20:20:48 +0100

    filedestriptor is 1

--- ft_print_udecimal.c
+++ ft_print_udecimal.c
@@ -6,21 +6,21 @@
 /*   By: vpogorel <vpogorel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 14:00:59 by vpogorel          #+#    #+#             */
-/*   Updated: 2024/12/05 17:10:20 by vpogorel         ###   ########.fr       */
+/*   Updated: 2024/12/05 20:19:27 by vpogorel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	print_digit(int fd, unsigned int n, int sign, int *count)
+static void	print_digit(unsigned int n, int sign, int *count)
 {
 	char	digit;
 
 	if (n == 0)
 		return ;
-	print_digit(fd, n / 10, sign, count);
+	print_digit(n / 10, sign, count);
 	digit = (n % 10) + '0';
-	write(fd, &digit, 1);
+	write(1, &digit, 1);
 	(*count)++;
 	return ;
 }
@@ -30,7 +30,7 @@ void	ft_print_udecimal(va_list args, int *count)
 	unsigned int	n;
 
 	n = va_arg(args, unsigned int);
-	print_digit(1, n, 1, count);
+	print_digit(n, 1, count);
 	if (n == 0)
 	{
 		write(1, "0", 1);
